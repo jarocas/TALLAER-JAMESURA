3db1f0787647c721b3d1a684e268801b0cba205d
james rodriguez <jamesrocas18@gmail.com>
2022-09-16 16:22:19 -0500

    cambio ingrese por digite

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -10,7 +10,7 @@ for i in range(5):
     fruta={}
     fruta['nombre']=input("Digite el nombre de la fruta: ")
     fruta['color']=input("Digite el color de la fruta: ")
-    fruta['precio']=input("Ingrese el precio de la fruta: ")
+    fruta['precio']=input("Digite el precio de la fruta: ")
     frutas.append(fruta)
     
 print(frutas)
